a0a4b85161092d874c1800476a83a849f0147dcd
nadyi2001 <1234554321nch@gmail.com>
2024-05-22 17:19:32 +0400

    Add files via upload

---
+++ src/main/java/ru/sgu/task1/t1.java
@@ -0,0 +1,8 @@
+package task1;
+
+public class t1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(System.getProperty("user.dir"));
+    }
+}
