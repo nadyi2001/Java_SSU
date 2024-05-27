6ebcd106e026fa20abedfa2a4a51b11aa4656723
nadyi2001 <1234554321nch@gmail.com>
2024-05-27 21:35:39 +0400

    Add files via upload

--- src/task3/t3/Main.java
+++ src/task3/t3/Main.java
@@ -3,13 +3,16 @@ package task3.t3;
 public class Main {
 
     public void run(){
-        TreeType oakType = TreeFactory.getTreeType("Oak", "Green", "Rough");
-        TreeType pineType = TreeFactory.getTreeType("Pine", "Green", "Smooth");
+        TreeType oakType = new TreeType("Oak", "Green", "Rough");
+        TreeType pineType = new TreeType("Pine", "Green", "Smooth");
+        TreeType pineType111 = new TreeType(null, "", "");
 
         Tree tree1 = new Tree(10, 20, oakType);
         Tree tree2 = new Tree(15, 25, oakType);
-        Tree tree3 = new Tree(30, 40, pineType);
-
+        Tree tree3 = new Tree(30, 40, pineType111);
+        new TreeFactory().getTreeType("12", "1", null);
+        new TreeFactory().getTreeType("12", null, "233");
+        new TreeFactory().getTreeType(null, "1", "23");
         tree1.draw();
         tree2.draw();
         tree3.draw();
--- src/task3/t3/TreeFactory.java
+++ src/task3/t3/TreeFactory.java
@@ -4,9 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TreeFactory {
-    private static Map<String, TreeType> treeTypes = new HashMap<>();
+    private Map<String, TreeType> treeTypes = new HashMap<>();
 
-    public static TreeType getTreeType(String name, String color, String texture) {
+    private void checkStrings(String str1, String str2, String str3) {
+        StringBuilder sb = new StringBuilder();
+        boolean hasNull = ((str1 == null) || (str2 == null) || (str3 == null));
+        if (hasNull) {
+            throw new NullPointerException("At least one of the strings is null. " + sb.toString());
+        } else {
+            System.out.println("None of the strings are null. " + sb.toString());
+        }
+    }
+
+    public TreeType getTreeType(String name, String color, String texture) {
+
+        try {
+            checkStrings(name, color, texture);
+        } catch (NullPointerException e){
+            e.printStackTrace();
+
+            System.out.println(String.format("Name: %s, Color: %s, Texture: %s", name, color, texture) + "\n");
+        }
         String key = name + "-" + color + "-" + texture;
         TreeType result = treeTypes.get(key);
         if (result == null) {
