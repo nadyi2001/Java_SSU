c228cd9fe0ccbc209f779a3780710a628800679e
nadyi2001 <1234554321nch@gmail.com>
2024-05-27 12:57:58 +0400

    Delete src/main/java/ru/sgu/task1 directory

--- src/main/java/ru/sgu/task1/t1.java
+++
@@ -1,8 +0,0 @@
-package task1;
-
-public class t1 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println(System.getProperty("user.dir"));
-    }
-}
